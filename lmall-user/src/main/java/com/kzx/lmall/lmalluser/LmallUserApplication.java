5531bd57947f4d4e7309d910cc8343bad56040ec
kzx <zkong1130@gmail.com>
2020-09-13 22:48:50 -0700

    Add version 1 in README.md

---
+++ lmall-user/src/main/java/com/kzx/lmall/lmalluser/LmallUserApplication.java
@@ -0,0 +1,13 @@
+package com.kzx.lmall.lmalluser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmallUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LmallUserApplication.class, args);
+    }
+
+}
