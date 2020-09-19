79743416cbe677b7aedf0230e6916bbc070a647f
kzx <zkong1130@gmail.com>
2020-09-18 20:51:18 -0700

    Add version 1 in README.md

--- lmall-user/src/main/java/com/kzx/lmall/lmalluser/LmallUserApplication.java
+++ lmall-user/src/main/java/com/kzx/lmall/lmalluser/LmallUserApplication.java
@@ -2,8 +2,10 @@ package com.kzx.lmall.lmalluser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.kzx.lmall.lmalluser.mapper")
 public class LmallUserApplication {
 
     public static void main(String[] args) {
